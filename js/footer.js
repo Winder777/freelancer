fe332a1a0e25db2b0a0c4c2cb6dc08705f5b2ac8
Winder777 <windermartinez0000001@gmail.com>
2022-08-16 02:56:32 -0400

    Actualización

--- js/footer.js
+++ js/footer.js
@@ -1,11 +1,13 @@
-document.write("<hr style=\"margin: 30pt 0pt 0pt;\">");
+/*document.write("<hr style=\"margin: 30pt 0pt 0pt;\">");
 document.write("<p style=\"margin: 10pt 100pt 0pt; text-align: justify; line-height: normal;\">");
-document.write("<i>");
-document.write("©[Copyright]");
-document.write("<\/i><\/p>");
+document.write("<i>©[Copyright]<\/i>");
+document.write();
+document.write("<\/p>");
 document.write('<p class="letras-footer">');
 document.write("<i>");
 document.write("Las obras publicadas en este sitio son de libre acceso para fines académicos y como un medio de difundir la");
 document.write("producción cultural y el conocimiento generados por autores universitarios o auspiciados por las universidades, pero que estos y la Editorial se reservan el derecho de autoría a todos los fines que correspondan.");
 document.write("<\/i><\/p>");
-document.write("<hr>");
+document.write("<hr>");*/
+
+document.getElementById('footer').innerHTML="Copyright © Clínica Equilibrium";
